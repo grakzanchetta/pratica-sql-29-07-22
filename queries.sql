8adc519005edbbb724374644adbc29bf764ec1eb
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-29 20:44:26 -0300

    4 tabelas .sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
+-- Observação:
+
+--As queries a seguir foram feitas na ordem que acredito que o banco de dados deveria ser montado.
+
 CREATE DATABASE "customer_database";
 
 CREATE TABLE states (
@@ -11,4 +15,17 @@ name VARCHAR(40) NOT NULL UNIQUE,
 "stateId" INTEGER REFERENCES states(id)
 );
 
+CREATE TABLE customers (
+id SERIAL NOT NULL PRIMARY KEY,
+name TEXT NOT NULL,
+cpf VARCHAR(11) NOT NULL UNIQUE,
+email TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL
+);
 
+CREATE TABLE "customerPhones" (
+id SERIAL NOT NULL PRIMARY KEY,
+"customerId" INTEGER REFERENCES customers(id),
+number VARCHAR (10) NOT NULL,
+type TEXT
+);
